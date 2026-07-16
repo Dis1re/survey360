1f98432afe2d77e30bc6b2de41446eba7b4f3a4e
R0ya1Wasp <roma031005@gmail.com>
2026-07-16 15:30:58 +0400

    пофикшенные группы

--- WebApp/Migrations/20260716100231_AddUserGroups.cs
+++ WebApp/Migrations/20260716100231_AddUserGroups.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,13 +11,32 @@ namespace WebApp.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "UserGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    UserIds = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedByUserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroups_CreatedByUserId",
+                table: "UserGroups",
+                column: "CreatedByUserId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "UserGroups");
+            migrationBuilder.Sql("DROP TABLE IF EXISTS \"UserGroups\";");
         }
     }
 }
